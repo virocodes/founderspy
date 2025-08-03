e457191c0c8c0bdcddc553f2543122c6c4deadbf
Dharshan Bellan <dbellan1291@gmail.com>
2025-08-02 21:17:19 -0700

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "founderspy",
   description: "search through every yc founder",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
